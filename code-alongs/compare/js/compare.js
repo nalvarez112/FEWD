b8746ae9bf710a575d4ed1e530a8c1ee86ecbea6
nalvarez112 <nickalvarez112@gmail.com>
2016-05-26 18:39:01 -0700

    homework refresh

--- code-alongs/compare/js/compare.js
+++ code-alongs/compare/js/compare.js
@@ -5,11 +5,38 @@ var compare = document.querySelector(".compare");
 
 // EVENTS
 
+// Listen for the compare button to be clicked
+
 compare.addEventListener('click', compareNumbers);
 
 
+
 // EVENT HANDLERS
+//----------------------------
+// Compare first and second number
+// Update the page - change the sign
 
 function compareNumbers(){
-	sign.textContent = "="
+
+	var firstValue = firstNumber.value;
+	firstValue = parseInt(firstValue);
+	console.log('first:', firstValue);
+
+	var secondValue = secondNumber.value;
+	secondValue = parseInt(secondValue);
+	console.log('second:', secondValue);
+
+	// JS Comparison
+
+	if (firstValue > secondValue) {
+		sign.textContent = ">";
+
+	} else if (firstValue < secondValue) {
+		sign.textContent = "<";
+
+	} else {
+		sign.textContent = "=";
+	}
+
 }
+
