413eefe06976200099aeebfd0f4aa631cdf6b53b
nalvarez112 <nickalvarez112@gmail.com>
2016-05-31 21:05:15 -0700

    accidently deleted temp converter

--- code-alongs/travel-news/js/travel-news.js
+++ code-alongs/travel-news/js/travel-news.js
@@ -1,7 +1,27 @@
+
+// STRUCTURE
+// -------------------------------
 var parent = document.querySelector(".sponsored-articles");
 
-var child = document.createElement("li");
 
-parent.appendChild(child);
 
-child.textContent = "text text tex text";
+// EVENT HANDLER
+// -------------------------------
+
+function createItem(){
+
+	// STEP1 CREATE
+	
+	var child = document.createElement("li");
+	
+	// STEP 2 ADD CONTENT AND ATTRIBUTES
+	
+	child.textContent = "text text tex text";
+	child.classList.add('new');
+	child.setAttribute('src', 'img/logo.jpg')
+	
+	// STEP 3 ADD TO DOM TREE
+	
+	parent.appendChild(child);
+
+}
